89d967b8ffcb8a0ba40ca441010f618efc12b656
i.nasirov <ilyasnasirov9@gmail.com>
2024-08-27 17:42:48 +0500

    v2

--- src/main/java/com/example/university/mapper/StudentMapper.java
+++ src/main/java/com/example/university/mapper/StudentMapper.java
@@ -3,9 +3,11 @@ package com.example.university.mapper;
 import com.example.university.entity.Student;
 import com.example.university.dto.StudentDto;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.MappingConstants;
 import org.mapstruct.factory.Mappers;
 
-@Mapper
+@Mapper(componentModel = MappingConstants.ComponentModel.SPRING)
 public interface StudentMapper {
 
     StudentMapper INSTANCE= Mappers.getMapper(StudentMapper.class);
